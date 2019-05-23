9036cda374b2c859fc935bbd5204f60974348934
Alexander Riccio <test35965@gmail.com>
2019-05-23 00:32:00 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -4,6 +4,12 @@
 def key_for_min_value(name_hash)
   smallest_key = nil
   name_hash.each do |name, value|
-    
+    if smallest_key == nil
+      smallest_key = name
+    end
+    if value < name_hash[smallest_key]
+      smallest_key = name
+    end
   end
+  smallest_key
 end
